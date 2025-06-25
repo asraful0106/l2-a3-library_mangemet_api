7bf012f83fc486698888d46b3794418682d1a4ef
Md. Asraful Alom <asrafulapf@gmail.com>
2025-06-25 15:51:46 +0600

    feat(API): endpoint to get all books and filtered books has been done

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -2,6 +2,57 @@ import { Request, Response } from "express";
 import { z } from 'zod/v4';
 import Books from "../models/book.modle";
 
+
+// For getting all books
+const getAllBooks = async (req: Request, res: Response) => {
+    const zQuery = z.object({
+        filter: z.string().optional(),
+        sortBy: z.string().optional(),
+        sort: z.string().optional(),
+        limit: z.string().optional(),
+    });
+
+    try {
+        const queryData = zQuery.parse(req.query);
+        try {
+            // For no filtering on data
+            if (!queryData.filter && !queryData.sortBy && !queryData.sort && !queryData.limit) {
+                const allBook = await Books.find();
+                res.status(200).json({
+                    "success": true,
+                    "message": "Books retrieved successfully",
+                    "data": allBook
+                })
+            }
+
+            // For filterning the data
+            const filteredBook = await Books.find(queryData.filter ? { 'genre': queryData.filter } : {})
+                .sort(queryData.sortBy && queryData.sort ? { [queryData.sortBy]: queryData.sort == 'asc' ? 1 : queryData.sort == 'desc' ? -1 : 1 } : {})
+                .limit(parseInt(queryData.limit || '0'));
+            res.status(200).json({
+                "success": true,
+                "message": "Books retrieved successfully",
+                "data": filteredBook
+            });
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json({
+                "message": "Internal Server Error!",
+                "success": false,
+                "error": err
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({
+            "message": "Validation Error",
+            "success": false,
+            "error": err
+        });
+    }
+}
+
+// For creating a new book entry
 const createNewBook = async (req: Request, res: Response) => {
     const zBook = z.object({
         title: z.string(),
@@ -23,7 +74,7 @@ const createNewBook = async (req: Request, res: Response) => {
                 "message": "Book created successfully",
                 "data": newBook
             });
-        } catch (err:any) {
+        } catch (err: any) {
             console.log(err);
             return res.status(500).json({
                 "message": err?.errorResponse?.errmsg || "Internal Server Error!",
@@ -41,4 +92,4 @@ const createNewBook = async (req: Request, res: Response) => {
     }
 }
 
-export { createNewBook };
+export { getAllBooks, createNewBook };
--- src/routers/book.route.ts
+++ src/routers/book.route.ts
@@ -1,7 +1,12 @@
 import { Request, Response, Router } from 'express';
-import { createNewBook } from '../controllers/book.controller';
+import { createNewBook, getAllBooks } from '../controllers/book.controller';
 
 const bookRouter: Router = Router();
+
+// For Getting All Books
+bookRouter.get('/', async(req: Request, res: Response) =>{
+    await getAllBooks(req, res);
+})
 
 // For creating a new Book
 bookRouter.post('/', async (req: Request, res: Response) => {
